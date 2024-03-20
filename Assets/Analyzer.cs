e037a6372ad44e83a63c79773636391198e32e0d
vik <39262485+ViktorKornilov@users.noreply.github.com>
2024-03-20 19:04:18 +0200

    Analyzer

---
+++ Assets/Analyzer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Analyzer : MonoBehaviour
+{
+    AudioSource source;
+    public UnityEvent<float> onVolumeChanged;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        var samples = new float[735];
+        source.clip.GetData(samples, source.timeSamples);
+
+        var sum = 0f;
+        for (int i = 0; i < samples.Length; i++)
+        {
+            sum += Mathf.Abs(samples[i]);
+        }
+
+        var average = sum / samples.Length;
+
+        onVolumeChanged.Invoke(average);
+    }
+}
