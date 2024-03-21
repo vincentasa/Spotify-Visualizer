5e98f6d682d18f31ad639198da58f147f3dd587a
vincentasa <vincentas.augustas@gmail.com>
2024-03-21 19:46:58 +0200

    added visualiser

--- Assets/Scripts/Dancer.cs
+++ Assets/Scripts/Dancer.cs
@@ -15,7 +15,7 @@ public class Dancer : MonoBehaviour
 
     public void Dance(float volume)
     {
-        transform.localScale =Vector3.one * (0.5f + Mathf.Pow(0.5f + volume,power)*maxSize);
+        transform.localScale = Vector3.one * (0.5f + Mathf.Pow(0.5f + volume,power)*maxSize);
 
         var mixedColor = Color.Lerp(startColor, endColor, volume * 5f);
         GetComponent<MeshRenderer>().material.SetColor("_EmissionColor", mixedColor * 5);
