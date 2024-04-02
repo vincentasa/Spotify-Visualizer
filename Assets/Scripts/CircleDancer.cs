7707267cef01fbf3905143aa8df4f26ea06ac779
vincentasa <vincentas.augustas@gmail.com>
2024-04-02 22:02:53 +0300

    Update CircleDancer.cs

--- Assets/Scripts/CircleDancer.cs
+++ Assets/Scripts/CircleDancer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class CircleDancer : MonoBehaviour
+public class CircleDancer :MonoBehaviour
 {
     public GameObject prefab;
     public int count = 10;
